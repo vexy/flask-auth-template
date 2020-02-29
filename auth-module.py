cb7c3b724cfc234d25686b8198359b4bdd39f6ff
Veljko Tekelerović <veljko.tekelerovic@gmail.com>
2020-02-29 20:47:31 +0100

    Added initial routes sceleton

--- auth-module.py
+++ auth-module.py
@@ -4,6 +4,19 @@ from flask import Flask
 if __name__ == '__main__':
     app = Flask(__name__)
 
+# ROUTES DEFINITION:
+@app.route('/unprotected')
+def unprotected():
+    return 'This is unprotected area'
+
+@app.route('/protected')
+def protected():
+    return 'This is protected area'
+
+@app.route('/login')
+def login():
+    return 'You are now logged in'
+
 # start the server
 if __name__ == '__main__':
     app.run(debug=True)
