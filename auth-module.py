32f48d7605612d95436116d86d66c68b581e0cde
Veljko Tekelerović <veljko.tekelerovic@gmail.com>
2020-02-29 21:16:17 +0100

    Login method coding
    Added basic password check
    Initial response decoding
    Custom secret_key implementation

--- auth-module.py
+++ auth-module.py
@@ -1,8 +1,12 @@
-from flask import Flask
+from flask import Flask, jsonify, request, make_response
+import jwt
+import datetime
 
 # initialize main Flask object
 if __name__ == '__main__':
     app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'some_secret_key'
 
 # ROUTES DEFINITION:
 @app.route('/unprotected')
@@ -15,7 +19,18 @@ def protected():
 
 @app.route('/login')
 def login():
-    return 'You are now logged in'
+    auth = request.authorization
+    if auth and auth.password == 'password':
+        token_expiration = str(datetime.datetime.utcnow() + datetime.timedelta(minutes=30))
+        token = jwt.encode(
+        {
+            'user': auth.username,
+            'expiration': token_expiration
+        }, app.config['SECRET_KEY'], algorithm='HS256')
+
+        return jsonify({'token': token.decode('UTF-8')})
+
+    return make_response('Could not verify!', 401, {'WWW-Authenticate': 'Basic realm="Login required"'})
 
 # start the server
 if __name__ == '__main__':
